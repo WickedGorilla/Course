fd284771541ec03f912aec21bd387134766ea514
wickedgorilla <nikita.1999.kotov@gmail.com>
2020-08-29 23:08:03 +0400

    Revert "Task2 complete"
    This reverts commit dfbfa3dbec9f5389c34d63325449fc80ea83d484.

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
