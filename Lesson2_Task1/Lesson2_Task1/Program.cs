5db709b7b91c30a49aed1f94d94d9cb456893bc1
wickedgorilla <wickedgorilla@outlook.com>
2020-09-27 17:59:23 +0400

    upd

---
+++ Lesson2_Task1/Lesson2_Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lesson2_Task1
+{
+    abstract class Character
+    {
+        public int Health { get; private set; }
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                Console.WriteLine("Я умер");
+            }
+        }
+
+    }
+
+    class Wombat : Character
+    {
+        public int Armor { get; private set; }
+
+        public override void TakeDamage(int damage) 
+        {
+            int calculatedDamage = damage - Armor;
+            base.TakeDamage(calculatedDamage);
+        }
+    }
+
+    class Human : Character
+    {
+        public int Agility { get; private set; }
+
+        public override void TakeDamage(int damage)
+        {
+            int calculatedDamage = damage / Agility;
+            base.TakeDamage(calculatedDamage);
+        }
+    }
+}
