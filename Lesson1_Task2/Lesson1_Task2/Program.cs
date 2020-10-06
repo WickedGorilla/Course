251d38452a7880a3e1869d9690b97b37ede2b8f4
wickedgorilla <wickedgorilla@outlook.com>
2020-10-07 01:04:35 +0400

    Update Task

---
+++ Lesson1_Task2/Lesson1_Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Bag
+    {
+        public List<Item> Items { get; private set; }
+        public int MaxWeidth { get; private set; }
+
+        public void AddItem(string name, int count)
+        {
+            int currentWeidth = Items.Sum(item => item.Count);
+
+            if (currentWeidth + count > MaxWeidth) return;
+
+            Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+            Item newItem;
+
+            if (targetItem != null)
+            {
+                newItem = new Item(name, targetItem.Count + count);
+                Items.Remove(targetItem);
+            }
+            else
+                newItem = new Item(name, count);
+
+            Items.Add(newItem);
+        }
+
+        public Item GetItem(string name)
+        {
+            return Items.Find(item => item.Name == name);
+        }
+
+    }
+
+    class Item
+    {
+        public int Count { get; private set; }
+        public string Name { get; private set; }
+
+        public Item(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public void AddCount(int count)
+        {
+            Count += count;
+        }
+    }
+
+}
